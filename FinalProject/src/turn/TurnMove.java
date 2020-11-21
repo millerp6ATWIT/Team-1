b6b95073914b7cb079de3144b13ee6d092b5f5c0
dunnm10 <dunnm10@BY2015A88002174.wit.private>
2020-11-21 15:03:43 -0500

    isLegal methods in TurnMove and TurnUse

--- FinalProject/src/turn/TurnMove.java
+++ FinalProject/src/turn/TurnMove.java
@@ -1,6 +1,9 @@
 package turn;
 
-import entity.Actor;
+import java.util.ArrayList;
+
+import entity.*;
+import game.Level;
 
 public class TurnMove extends Turn {
 	private int[] destination;
@@ -12,5 +15,19 @@ public class TurnMove extends Turn {
 	// set position of actor to destination
 	public void execute(Actor a) {
 		a.setPosition(destination);
+	}
+	
+	// returns a boolean on if a move is legal
+	public boolean isLegal(Level l) {
+		ArrayList<Entity> e = l.entitiesAt(destination);
+		for(int i = 0; i < e.size(); i++) {
+			if(e.get(i) instanceof Tile) {
+				Tile t = (Tile) e.get(i);
+				if(t.getImpassable() == true) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
--- FinalProject/src/turn/TurnUse.java
+++ FinalProject/src/turn/TurnUse.java
@@ -2,6 +2,7 @@ package turn;
 
 import entity.Actor;
 import item.Item;
+import java.util.ArrayList;
 
 public class TurnUse extends Turn {
 	private Item toUse;
@@ -13,5 +14,15 @@ public class TurnUse extends Turn {
 	// apply the effects of toUse to the specified actor
 	public void execute(Actor a) {
 		toUse.use(a);
+	}
+	
+	public boolean isLegal() {
+		ArrayList<Item> inventory = ((Actor) toUse.getOwner()).getInventory();
+		for(int i = 0; i < inventory.size(); i++) {
+			if(inventory.get(i).equals(toUse)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
